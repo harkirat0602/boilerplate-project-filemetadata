bae2983e59ac08921d5fba5ae5a030fbc7d6695a
Harkirat Singh <harkirat.kirat0602@gmail.com>
2024-06-06 12:02:53 +0530

    first attempt

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+const multer = require("multer")
+const upload = multer({dest:'public/'})
 
 var app = express();
 
@@ -10,6 +12,22 @@ app.use('/public', express.static(process.cwd() + '/public'));
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
+
+
+app.post('/api/fileanalyse',upload.single('upfile'),(req,res)=>{
+  if(!req.file){
+    return res.status(400).send("Error")
+  }
+
+  return res
+  .status(200)
+  .json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  })
+
+})
 
 
 
